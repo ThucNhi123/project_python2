cbf7d9b912198cdb46ae5e701ea68e23440d4ef4
duynguyen260406-source <duynguyen260406@gmail.com>
2025-11-18 22:21:44 +0700

    duy refactor static files settings in settings.py
    Updated STATIC_URL and STATICFILES_DIRS settings for static files management.

--- burn_calories/settings.py
+++ burn_calories/settings.py
@@ -117,26 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 STATIC_URL = '/static/'
-import os
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'tracker/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "tracker" / "static",
 ]
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = BASE_DIR / "staticfiles"
